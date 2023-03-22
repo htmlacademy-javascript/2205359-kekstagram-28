e2af51bf4fcee00330cf17da49481801a0eef3b0
Мария <marklukanova@gmail.com>
2023-03-22 21:54:04 +0300

    Вносит правки

--- js/form.js
+++ js/form.js
@@ -2,28 +2,27 @@ import {validateForm} from './validate-form.js';
 import { extractDigits } from './string-util.js';
 import {sliderFieldset, createSlider, onEffectsListClick, destroySlider} from './form-sliders.js';
 
+const MIN_SCALE_AMOUNT = 25;
+const MAX_SCALE_AMOUNT = 100;
+const SCALE_STEP = 25;
+
 const uploadForm = document.querySelector('.img-upload__form');
 const uploadOverlay = uploadForm.querySelector('.img-upload__overlay');
 const imgUploadInput = uploadForm.querySelector('.img-upload__input');
 const closeFormBtn = uploadForm.querySelector('.img-upload__cancel');
-
 const scaleControlValue = uploadForm.querySelector('.scale__control--value');
-const scaleDownBtn = uploadForm.querySelector('.scale__control--smaller');
-const scaleUpBtn = uploadForm.querySelector('.scale__control--bigger');
+const scaleControls = uploadForm.querySelector('.img-upload__scale');
 const imgUploadContainer = uploadForm.querySelector('.img-upload__preview');
 const imgUploadPrewiev = imgUploadContainer.querySelector('img');
 const effectsList = uploadForm.querySelector('.effects__list');
 
-const onScaleDownClick = () => {
-  if (extractDigits(scaleControlValue.value) > 25) {
-    scaleControlValue.value = `${extractDigits(scaleControlValue.value) - 25}%`;
+const onScaleControlsClick = (evt) => {
+  if (evt.target.closest('.scale__control--smaller') && extractDigits(scaleControlValue.value) > MIN_SCALE_AMOUNT) {
+    scaleControlValue.value = `${extractDigits(scaleControlValue.value) - SCALE_STEP}%`;
     imgUploadPrewiev.style.transform = `scale(${extractDigits(scaleControlValue.value) / 100})`;
   }
-};
-
-const onScaleUpClick = () => {
-  if (extractDigits(scaleControlValue.value) < 100) {
-    scaleControlValue.value = `${extractDigits(scaleControlValue.value) + 25}%`;
+  if (evt.target.closest('.scale__control--bigger') && extractDigits(scaleControlValue.value) < MAX_SCALE_AMOUNT) {
+    scaleControlValue.value = `${extractDigits(scaleControlValue.value) + SCALE_STEP}%`;
     imgUploadPrewiev.style.transform = `scale(${extractDigits(scaleControlValue.value) / 100})`;
   }
 };
@@ -41,8 +40,7 @@ function onImageUpload () {
   uploadOverlay.classList.remove('hidden');
   sliderFieldset.classList.add('hidden');
   createSlider();
-  scaleDownBtn.addEventListener('click', onScaleDownClick);
-  scaleUpBtn.addEventListener('click', onScaleUpClick);
+  scaleControls.addEventListener('click', onScaleControlsClick);
   effectsList.addEventListener('change', onEffectsListClick);
   uploadForm.addEventListener('submit', onFormSubmit);
   closeFormBtn.addEventListener('click', imgUploadClose);
@@ -57,8 +55,7 @@ function imgUploadClose () {
   imgUploadPrewiev.style.filter = '';
   imgUploadPrewiev.removeAttribute('class');
   destroySlider();
-  scaleDownBtn.removeEventListener('click', onScaleDownClick);
-  scaleUpBtn.removeEventListener('click', onScaleUpClick);
+  scaleControls.removeEventListener('click', onScaleControlsClick);
   effectsList.removeEventListener('change', onEffectsListClick);
   uploadForm.removeEventListener('submit', onFormSubmit);
   closeFormBtn.removeEventListener('click', imgUploadClose);
